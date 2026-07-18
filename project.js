6acc604e0a7b5bca9ab361975044fb3f58dd4ce0
unknown <noosaatefghaly112006@gmail.com>
2026-07-18 06:14:28 +0300

    First Portfolio

---
+++ project.js
@@ -0,0 +1,12 @@
+function sucess(){
+    let name = document.getElementById("name").value.trim();
+    let email = document.getElementById("email").value.trim();
+    let subject = document.getElementById("subject").value.trim();
+    let message = document.getElementById("message").value.trim();
+
+    if(name === "" || email === "" || subject === "" || message === ""){
+        alert("Please fill in all fields.");
+    }
+    else{
+    alert("Sucess Message");
+}}
